bca14e2417625d3e8ced948ff8e2de874c8e0b6e
sanatankc <sanatan.iit@gmail.com>
2023-05-15 20:55:05 +0530

    [wip] homepage

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				accent:'#C07C47',
+				accent2:'#4A7C60', 
+				green: {
+					300: '#65B086',
+					400: '#4A7C60',
+					500: '#3C674F',
+					600: '#0A5E2F'
+				},
+				gray: {
+					300: '#E2E2E2',
+					500: '#D9DBE9'
+				},
+				red: {
+					500: '#A9342D',
+				}
+			}
+		},
+		fontFamily: {
+			'display': ['"MADE Gentle"', 'sans-serif'],
+			'sans': ['"Nunito Sans"', 'sans-serif'],
+		}
+	},
+	plugins: [],
+}
