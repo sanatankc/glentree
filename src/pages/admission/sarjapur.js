3d3d8b1353828c578c5f7bf0dc0e7b554a0a26bd
Sanatan <sanatankc@gmail.com>
2023-08-22 14:33:31 +0530

    redirect setup

---
+++ src/pages/admission/sarjapur.js
@@ -0,0 +1,3 @@
+export async function get({ params, redirect }) {
+  return redirect("https://admissions.glentreeacademy.com/cart?sid=3&formTypeId=1", 307);
+}
---
+++ src/pages/admission/whitefield.js
@@ -0,0 +1,4 @@
+export async function get({ params, redirect }) {
+
+  return redirect("https://admissions.glentreeacademy.com/cart?sid=2&formTypeId=1", 307);
+}
