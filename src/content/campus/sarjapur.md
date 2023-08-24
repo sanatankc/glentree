1de2437ee6f7c6c6e7c288694c3a4ce90f724d11
satyam <satyamchaudharydev@gmail.com>
2023-08-24 15:16:06 +0530

    tina cms testing

---
+++ src/content/campus/sarjapur.md
@@ -0,0 +1,14 @@
+---
+name: sarjapur
+title: Glentree - Sarjapur
+description: A sprawling green campus located within the heart of the city.
+bannerBg: /src/images/campus/gas/school.jpeg
+virtualCampugsLink:
+  https://www.google.com/maps/@12.8977429,77.6849464,3a,75y,349.51h,82.52t/data=!3m8!1e1!3m6!1sAF1QipOr5WfWNbbfic6rOHNb2iIGHo6vd3F9sSIuKilE!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOr5WfWNbbfic6rOHNb2iIGHo6vd3F9sSIuKilE%3Dw203-h100-k-no-pi-0-ya303.22165-ro0-fo100!7i10676!8i5338?hl=en-US&entry=ttu
+galleryImages: [{
+    image: "/src/images/campus/gas/school.jpeg"
+}]
+ 
+affiliationNo: '831088'
+---
+
---
+++ tina/config.ts
@@ -0,0 +1,96 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "456dfaea-a704-4520-a4f6-582676c2eb90", // Get this from tina.io
+  token: "b0b6abc51cb2bba93c5c50e1b5cdd6edffcfde69", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "school",
+        label: "school",
+        path: "src/content/school",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "campus",
+        label: "campus",
+        path: "src/content/campus",
+        fields: [
+          {
+            type: "string",
+            name: "name",
+            label: "Name",
+            required: true,
+          },
+          {
+            type: "image",
+            name: "bannerBg",
+            label: "Banner Image",
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "virtualCampusLink",
+            label: "VirtualCampusLink",
+            required: true,
+          },
+         
+          {
+            type: "string",
+            name: "affiliationNo",
+            label: "AffiliationNo",
+            required: true,
+          },
+          {
+            type: "object",
+            name: "galleryImages",
+            label: "Gallery Images",
+            list: true,
+            fields: [{ type: "image", name: "image", label: "My Image" }],
+          }
+        
+        ],
+      },
+    ],
+  },
+});
