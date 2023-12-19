5856637cd4e013aae69a5652ab0cebcbd93a7686
sanatankc <sanatan.iit@gmail.com>
2023-12-20 04:27:28 +0530

    CMS CHanges

--- src/content/campus/bannerghatta.md
+++ src/content/campus/bannerghatta.md
@@ -12,5 +12,62 @@ showcase:
     title: Embarking on a New Journey at our latest campus
   - image: /images/WhatsApp Image 2023-11-09 at 12.01.44_efca9afc.jpg
     title: Pooja Vibes at our New Campus
+aroundTheCampus:
+  description: "Explore our new campus, designed to enhance the learning experience with modern facilities and resources."
+  bulletPoints:
+    - bulletPoint: "Earth Lab"
+    - bulletPoint: "High-Tech Computer Lab"
+    - bulletPoint: "Premium Sports Facilities"
+    - bulletPoint: "Fun-Secure Play Area"
+  gallery:
+    - image: "/images/gab-facilities/campus1.png"
+      altTag: "Learning and Leisure"
+      title: "Learning and Leisure"
+    - image: "/images/gab-facilities/campus2.png"
+      altTag: "Campus Highlights"
+      title: "Campus Highlights"
+    - image: "/images/gab-facilities/campus3.png"
+      altTag: "3 acres Campus Area"
+      title: "3 acres Campus Area"
+    - image: "/images/gab-facilities/receptionArea.jpg"
+      altTag: "Reception Area"
+      title: "Reception Area"
+    - image: "/images/gab-facilities/sportsFacilities.png"
+      altTag: "Sports Facilities"
+      title: "Sports Facilities"
+    - image: "/images/gab-facilities/playArea.jpg"
+      altTag: "Play Area"
+      title: "Play Area"
+    - image: "/images/gab-facilities/computerLab.jpg"
+      altTag: "High-Tech Computer lab"
+      title: "High-Tech Computer lab"
+    - image: "/images/gab-facilities/earthLab.png"
+      altTag: "Earth Lab"
+      title: "Earth Lab"
+    - image: "/images/gab-facilities/swimmingPool.jpeg"
+      altTag: "Swimming Pool"
+      title: "Swimming Pool"
+    - image: "/images/gab-facilities/basketBall.jpeg"
+      altTag: "BasketBall Court"
+      title: "BasketBall Court"
+heroImages:
+  - image_desktop: "/images/gab-facilities/campus2.png"
+    image_mobile: "/images/gab-facilities/campus2.png"
+    alt: "Campus View"
+    position: "center"
+  - image_desktop: "/images/gab-facilities/earthLab.png"
+    image_mobile: "/images/gab-facilities/earthLab.png"
+    alt: "Earth Lab"
+    position: "center"
+  - image_desktop: "/images/gab-facilities/campus1.png"
+    image_mobile: "/images/gab-facilities/campus1.png"
+    alt: "Campus View"
+    position: "center"
+contactUs:
+  phoneNo: '+91 8105826950'
+  mapEmbedLink: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15559.921740228929!2d77.5994554!3d12.8445401!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6be1a2dd9ebb%3A0x9fb79f397d5fedd2!2sGlentree%20Academy%20Bannerghatta!5e0!3m2!1sen!2sin!4v1695325697476!5m2!1sen!2sin'
+  address: 'C K Palya Rd, Hommadevanahalli, Bengaluru, Doddakammanahalli, Karnataka 560083'
+  workingHours: 'Monday to Saturday - 8:00 AM to 4:00 PM'
+  email: admission.gab@glentreeacademy.com
 ---
 
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,14 @@ import { z, defineCollection } from 'astro:content';
 const campusCollection = defineCollection({
   type: 'content', 
   schema: ({image}) => z.object({
+    heroImages: z.array(
+      z.object({
+        image_desktop: z.string(),
+        image_mobile: z.string(),
+        alt: z.string(),
+        position: z.string(),
+      })
+    ).optional(),
     title: z.string().optional(),
     description: z.string().optional(),
     bannerBg: z.string().optional(),
@@ -11,30 +19,67 @@ const campusCollection = defineCollection({
     showcase: z.any().optional(),
     affiliationNo: z.string().optional(),
     aroundTheCampus: z.object({
-      title: z.string(),
       description: z.string(),
-      ctaText: z.string(),
+      bulletPoints: z.array(
+        z.object({
+          bulletPoint: z.string(),
+        })
+      ).optional(),
       gallery: z.array(
         z.object({
-          image: z.string().optional(),
+          image: z.string(),
+          altTag: z.string().optional(),
           title: z.string().optional(),
           description: z.string().optional(),
         })
-      )
+      ).optional(),
     }).optional(),
+    contactUs: z.object({
+      phoneNo: z.string(),
+      email: z.string(),
+      mapEmbedLink: z.string(),
+      address: z.string(),
+      workingHours: z.string(),
+    }),
   }),
 });
+
 const homeCollection = defineCollection({
   type: 'content', 
   schema: () => z.object({
-    title: z.string(),
+    title: z.string().optional(),
     heroImages: z.array(
       z.object({
         image_desktop: z.string().optional(),
         image_mobile: z.string().optional(),
         position: z.string().optional(),
-      }),
-    )
+      })
+    ).optional(),
+    testimonials: z.array(
+      z.object({
+        testimonial_picture: z.string().optional(),
+        testimonial_picture_alt: z.string().optional(),
+        testimonial: z.string().optional(), // 'rich-text' translates to 'any' in Zod
+        author: z.string().optional(),
+        role: z.string().optional(),
+        embedId: z.string().optional(),
+      })
+    ).optional(),
+    bigAdmissionCTA: z.object({
+      subtitle: z.string().optional(),
+      heading: z.string().optional(),
+      body: z.string().optional(),
+      primaryCTAText: z.string().optional(),
+      primaryCTALink: z.string().optional(),
+      secondaryCTALink: z.string().optional(),
+      secondaryCTAText: z.string().optional(),
+    }).optional(),
+    faqs: z.array(
+      z.object({
+        question: z.string(),
+        answer: z.string(),
+      })
+    ).optional(),
   }),
 });
 
@@ -69,6 +114,90 @@ const updateCollection = defineCollection({
   }),
 })
 
+const curriculumCollection = defineCollection({
+  type: 'content',
+  schema: () => z.object({
+    academics: z.array(
+      z.object({
+        title: z.string(),
+        grade: z.string(),
+        concepts: z.array(
+          z.object({
+            concept: z.string(),
+          })
+        ),
+        image: z.string().optional(),
+        imageAlt: z.string().optional(),
+        newlyAdded: z.boolean().optional(),
+      })
+    ),
+  }),
+});
+
+const afterschoolCollection = defineCollection({
+  type: 'content',
+  schema: () => z.object({
+    afterschool: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        thumbnail: z.string(),
+        thumbnailAlt: z.string(),
+      })
+    ),
+  }),
+});
+
+
+const clubsCollection = defineCollection({
+  type: 'content',
+  schema: () => z.object({
+    clubs: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        thumbnail: z.string(),
+        thumbnailAlt: z.string(),
+      })
+    ),
+  }),
+});
+
+const coscholasticsCollection = defineCollection({
+  type: 'content',
+  schema: () => z.object({
+    coscholastics: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        thumbnail: z.string(),
+        thumbnailAlt: z.string(),
+      })
+    ),
+  }),
+});
+
+
+const admissionsCollection = defineCollection({
+  type: 'content',
+  schema: () => z.object({
+    admissionProcess: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string().optional(),
+      })
+    ),
+    keyInformation: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+      })
+    ),
+  }),
+});
+
+
 export const collections = {
   'school': blogCollection,
   'campus': campusCollection,
@@ -76,4 +205,9 @@ export const collections = {
   'disclosure': disclosureCollection,
   'blog': blogCollection,
   'update': updateCollection,
+  'curriculum': curriculumCollection,
+  afterschool: afterschoolCollection,
+  clubs: clubsCollection,
+  coscholastics: coscholasticsCollection,
+  admissions: admissionsCollection,
 };
